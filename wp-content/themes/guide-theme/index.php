15ff6e9ff49a14dca6f42c8a96ee601a8554ecf1
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-05-29 00:03:38 +0200

    Link theme with downloaded bootstrap, jquery and jquery.validate

--- wp-content/themes/guide-theme/index.php
+++ wp-content/themes/guide-theme/index.php
@@ -14,8 +14,8 @@
     <meta name="author" content="Wojciech Chrzastek" />
     <meta name="copyright" content="Wojciech Chrzastek" />
     <meta name="robots" content="index, follow" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
-    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/styles.css" />
 
     <title>Guide wp-theme</title>
 </head>
@@ -198,12 +198,11 @@
         <p class="font-weight-light">&copy;<script>document.write(new Date().getFullYear())</script> Wojciech Chrzastek</p>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/1cc53aee27.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" integrity="sha256-sPB0F50YUDK0otDnsfNHawYmA5M0pjjUf4TvRJkGFrI=" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery-3.5.1.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery.validate.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+    <!-- <script src="ttps://kit.fontawesome.com/1cc53aee27.js" crossorigin="anonymous"></script> -->
 
 </body>
 
