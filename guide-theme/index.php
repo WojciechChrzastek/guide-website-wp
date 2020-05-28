3f5d3aeaf42c781a927860484af7252c4fc18990
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-05-28 20:06:58 +0200

    Create wp theme structure from previously prepared files

---
+++ guide-theme/index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- <script async src=""></script>
+    <script></script> -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="Guide wp-theme" />
+    <meta name="keywords" content="guide wp-theme" />
+    <meta name="author" content="Wojciech Chrzastek" />
+    <meta name="copyright" content="Wojciech Chrzastek" />
+    <meta name="robots" content="index, follow" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/styles.css" />
+
+    <title>Guide wp-theme</title>
+</head>
+
+<body data-spy="scroll" data-target="#navbar" data-offset="120">
+
+    <header>
+        <nav id="navbar" class="navbar fixed-top navbar-expand-sm navbar-dark bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#top">
+                    <div>Wojciech Chrzastek<small class="font-weight-light"><br>Malaga guide</small></div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#trips">Trips</a>
+                        <a class="nav-item nav-link" href="#news">News</a>
+                        <a class="nav-item nav-link" href="#about-me">About&nbsp;me</a>
+                        <a class="nav-item nav-link" href="#contact">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <button onclick="topFunction()" id="scrollButton" title="Scroll up"><i class="fas fa-angle-up"></i></i></button>
+
+    <section id="top" class="container-fluid">
+        <div class="background-image" style="background-image: url('img/malaga-panoramic.jpg');"></div>
+        <h1>Welcome to Malaga!!</h1>
+        <p>
+            Snow topgallant nipperkin scuttle mizzenmast jib cog Spanish Main pressgang salmagundi trysail bilge water.<br> Handsomely snow knave? Capstan Yellow Jack weigh anchor!
+        </p>
+        <div class="scroll-icon d-none d-xl-block d-lg-block d-md-block">
+            <span class="scroll-icon-dot"></span>
+        </div>
+    </section>
+
+        <section id="trips" class="container-fluid">
+        <h2>Trips</h2>
+        <p>
+            Hearties gangplank heave down port heave to hogshead coxswain hail-shot walk the plank wherry. Ballast scuttle Gold Road Jolly Roger bounty overhaul red ensign bucko deadlights transom. Scuttle cog Davy Jones' Locker draft brig Shiver me timbers Chain Shot boatswain landlubber or just lubber handsomely. Wherry stern line coxswain Letter of Marque me strike colors chandler coffer lee. Pirate Round come about sutler Blimey driver matey aye hearties fathom haul wind.
+        </p>
+        <ul class="row justify-content-center align-items-center text-center">
+            <li class="col-auto">
+                <a href="" target="_blank">
+                    <img src="img/m1.jpg" title="m1" alt="m1" class="img-fluid" />
+                </a>
+                <h4>Quarterdeck</h4>
+            </li>
+            <li class="col-auto">
+                <a href="" target="_blank">
+                    <img src="img/m2.jpg" title="m2" alt="m2"  class="img-fluid" />
+                </a>
+                <h4>Keelhaul</h4>
+            </li>
+            <li class="col-auto">
+                <a href="" target="_blank">
+                    <img src="img/m3.jpg" title="m3" alt="m3"  class="img-fluid" />
+                </a>
+                <h4>Boatswain</h4>
+            </li>
+            <li class="col-auto">
+                <a href="" target="_blank">
+                    <img src="img/m5.jpg" title="m4" alt="m4"  class="img-fluid" />
+                </a>
+                <h4>Jolly Roger</h4>
+            </li>
+            <li class="col-auto">
+                <a href="" target="_blank">
+                    <img src="img/m1.jpg" title="m5" alt="m5"  class="img-fluid" />
+                </a>
+                <h4>Wherry</h4>
+            </li>
+        </ul>
+    </section>
+
+    <section id="news" class="container-fluid">
+        <h2>News</h2>
+        <p>
+            Cable dead men tell no tales Sea Legs careen chandler pink fathom holystone. Interloper bilged on her anchor Privateer flogging case shot transom chandler rum. Fathom gangplank sheet topsail careen transom quarter pinnace.
+        </p>
+        <ul class="row  row justify-content-around text-center align-items-top news">
+            <li class="col">
+                <a href="" target="_blank">
+                    <img src="img/e1.png" title="e1" alt="e1" class="img-fluid" />
+                    <h4>Long boat</h4>
+                </a>
+                <p>
+                    Driver spike gun smartly hands coffer. Haul wind lugger mizzenmast strike colors Admiral of the Black Jolly Roger.
+                </p>
+            </li>
+            <li class="col">
+                <a href="" target="_blank">
+                    <img src="img/e2.png" title="e2" alt="e2" class="img-fluid" />
+                    <h4>Provost</h4>
+                </a>
+                <p>
+                    Swing the lead hands Davy Jones' Locker Pieces of Eight chantey wench. Pinnace black jack spirits gaff ho stern.
+                </p>
+            </li>
+            <li class="col">
+                <a href="" target="_blank">
+                    <img src="img/e3.png" title="e3" alt="e3" class="img-fluid" />
+                    <h4>Main sheet</h4>
+                </a>
+                <p>
+                    Maroon bilge American Main gangplank lateen sail spyglass. Hempen halter scuppers furl transom cog Sail ho.
+                </p>
+            </li>
+        </ul>
+    </section>
+
+    <section id="about-me" class="container-fluid">
+        <h2>About me</h2>
+        <div class="about-me">
+            <p>
+                Port keelhaul ballast quarterdeck fathom doubloon hands barque hardtack Jolly Roger. Flogging clipper draft matey avast sheet jack sloop Yellow Jack boatswain. Clap of thunder careen Sink me matey swab gangway crack Jennys tea cup Jack Ketch overhaul stern. Ballast measured fer yer chains aye maroon snow run a shot across the bow fathom Yellow Jack Jolly Roger landlubber or just lubber. Take a caulk skysail gunwalls Jack Ketch carouser fathom port Pieces of Eight lookout hands.
+            </p>
+            <figure class="figure">
+                <img src="img/wojciech-chrzastek-monaco.jpeg" title="wojciech-chrzastek" alt="wojciech-chrzastek" class="figure-img img-fluid rounded">
+                <figcaption class="figure-caption text-right">That's me during the Monaco F1 GP race weekend in 2019</figcaption>
+            </figure>
+            <p>
+                Sail ho quarterdeck maroon gally yawl tack come about draft bilge water chandler. Cutlass chase guns bilge water starboard hornswaggle hardtack jury mast chandler provost Chain Shot. Walk the plank heave down schooner parley carouser lanyard fathom Chain Shot loaded to the gunwalls doubloon.
+            </p>
+        </div>
+    </section>
+
+    <section id="contact" class="container-fluid">
+        <h2>Contact</h2>
+        <form class="col-sm-4" id="contact-form">
+            <p>
+                Feel free to use the form below to contact me.
+                <br>
+                Fields marked with an * are required
+            </p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <label for="name">Name*</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Enter name"
+                     required pattern="[A-z0-9À-ž\s]{2,50}" minlength="2" maxlength="50"
+                     title="Name should be 2 to 50 alphanumeric chacacters long.">
+                </div>
+                <div class="col-sm-6">
+                    <label for="email">Email address*</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Enter email"
+                     required minlength="6" maxlength="100" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                     title="Please enter valid email address.">
+                    <small class="form-text text-muted">Your email will not be shared with anyone.</small>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="message">Message*</label>
+                <textarea class="form-control" name="message" id="message" rows="3" placeholder="Enter message"
+                 required></textarea>
+            </div>
+
+            <!-- Super-simple honeypot antispam -->
+            <input type="hidden" name="honey">
+            <!-- Super-simple honeypot antispam -->
+
+            <div id="msg"></div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>
+
+    <footer>
+        <ul class="list-group list-group-horizontal">
+            <li>
+                <a href="https://github.com/WojciechChrzastek" target="_blank"><i class="fab fa-github-square"></i></a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/wojciech-chrzastek/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </li>
+        </ul>
+        <p class="font-weight-light">&copy;<script>document.write(new Date().getFullYear())</script> Wojciech Chrzastek</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/1cc53aee27.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js" integrity="sha256-sPB0F50YUDK0otDnsfNHawYmA5M0pjjUf4TvRJkGFrI=" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
