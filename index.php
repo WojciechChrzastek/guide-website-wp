1e7862bc6477ee8b8f26f2c754648ff814d0206e
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-20 21:15:40 +0200

    Use wp loop in index.php

--- index.php
+++ index.php
@@ -17,95 +17,14 @@ get_header();
         </div>
     </section>
 
-        <section id="trips" class="container-fluid">
-        <h2>Trips</h2>
-        <p>
-            Hearties gangplank heave down port heave to hogshead coxswain hail-shot walk the plank wherry. Ballast scuttle Gold Road Jolly Roger bounty overhaul red ensign bucko deadlights transom. Scuttle cog Davy Jones' Locker draft brig Shiver me timbers Chain Shot boatswain landlubber or just lubber handsomely. Wherry stern line coxswain Letter of Marque me strike colors chandler coffer lee. Pirate Round come about sutler Blimey driver matey aye hearties fathom haul wind.
-        </p>
-        <ul class="row justify-content-center align-items-center text-center">
-            <li class="col-auto">
-                <a href="" target="_blank">
-                    <img src="img/m1.jpg" title="m1" alt="m1" class="img-fluid" />
-                </a>
-                <h4>Quarterdeck</h4>
-            </li>
-            <li class="col-auto">
-                <a href="" target="_blank">
-                    <img src="img/m2.jpg" title="m2" alt="m2"  class="img-fluid" />
-                </a>
-                <h4>Keelhaul</h4>
-            </li>
-            <li class="col-auto">
-                <a href="" target="_blank">
-                    <img src="img/m3.jpg" title="m3" alt="m3"  class="img-fluid" />
-                </a>
-                <h4>Boatswain</h4>
-            </li>
-            <li class="col-auto">
-                <a href="" target="_blank">
-                    <img src="img/m5.jpg" title="m4" alt="m4"  class="img-fluid" />
-                </a>
-                <h4>Jolly Roger</h4>
-            </li>
-            <li class="col-auto">
-                <a href="" target="_blank">
-                    <img src="img/m1.jpg" title="m5" alt="m5"  class="img-fluid" />
-                </a>
-                <h4>Wherry</h4>
-            </li>
-        </ul>
-    </section>
+    <section id="section" class="container-fluid">
+   
+        <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : the_post(); ?> 
+        <?php the_content(); ?>
+        <?php endwhile; ?>
+        <?php endif; ?> 
 
-    <section id="news" class="container-fluid">
-        <h2>News</h2>
-        <p>
-            Cable dead men tell no tales Sea Legs careen chandler pink fathom holystone. Interloper bilged on her anchor Privateer flogging case shot transom chandler rum. Fathom gangplank sheet topsail careen transom quarter pinnace.
-        </p>
-        <ul class="row  row justify-content-around text-center align-items-top news">
-            <li class="col">
-                <a href="" target="_blank">
-                    <img src="img/e1.png" title="e1" alt="e1" class="img-fluid" />
-                    <h4>Long boat</h4>
-                </a>
-                <p>
-                    Driver spike gun smartly hands coffer. Haul wind lugger mizzenmast strike colors Admiral of the Black Jolly Roger.
-                </p>
-            </li>
-            <li class="col">
-                <a href="" target="_blank">
-                    <img src="img/e2.png" title="e2" alt="e2" class="img-fluid" />
-                    <h4>Provost</h4>
-                </a>
-                <p>
-                    Swing the lead hands Davy Jones' Locker Pieces of Eight chantey wench. Pinnace black jack spirits gaff ho stern.
-                </p>
-            </li>
-            <li class="col">
-                <a href="" target="_blank">
-                    <img src="img/e3.png" title="e3" alt="e3" class="img-fluid" />
-                    <h4>Main sheet</h4>
-                </a>
-                <p>
-                    Maroon bilge American Main gangplank lateen sail spyglass. Hempen halter scuppers furl transom cog Sail ho.
-                </p>
-            </li>
-        </ul>
-    </section>
-
-    <section id="about-me" class="container-fluid">
-        <h2>About me</h2>
-        <div class="about-me">
-            <p>
-                Port keelhaul ballast quarterdeck fathom doubloon hands barque hardtack Jolly Roger. Flogging clipper draft matey avast sheet jack sloop Yellow Jack boatswain. Clap of thunder careen Sink me matey swab gangway crack Jennys tea cup Jack Ketch overhaul stern. Ballast measured fer yer chains aye maroon snow run a shot across the bow fathom Yellow Jack Jolly Roger landlubber or just lubber. Take a caulk skysail gunwalls Jack Ketch carouser fathom port Pieces of Eight lookout hands.
-            </p>
-            <figure class="figure">
-                <img src="img/wojciech-chrzastek-monaco.jpeg" title="wojciech-chrzastek" alt="wojciech-chrzastek" class="figure-img img-fluid rounded">
-                <figcaption class="figure-caption text-right">That's me during the Monaco F1 GP race weekend in 2019</figcaption>
-            </figure>
-            <p>
-                Sail ho quarterdeck maroon gally yawl tack come about draft bilge water chandler. Cutlass chase guns bilge water starboard hornswaggle hardtack jury mast chandler provost Chain Shot. Walk the plank heave down schooner parley carouser lanyard fathom Chain Shot loaded to the gunwalls doubloon.
-            </p>
-        </div>
     </section>
 
     <section id="contact" class="container-fluid">
