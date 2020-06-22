bf479d9d338bf13eb7bbc5796e01c65961600ee2
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-22 11:39:07 +0200

    Add link to home and blog in 404.php

--- 404.php
+++ 404.php
@@ -7,32 +7,39 @@
 get_header();
 ?>
 
+<section>
+
 <main id="site-content" role="main">
 
 	<div class="section-inner thin error404-content">
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
-	<h1>Page Not Found</h1>
 
-		<h1 class="entry-title"><?php _e( 'Page Not Found'); ?></h1>
+		<h1 class="entry-title"><?php _e( 'Sorry... That page seems to be missing!'); ?></h1>
 
-		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', ); ?></p></div>
+		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found.', ); ?></p>
+			<p>Maybe you were looking for something else? Take a look at the links below:</p>
 
-		<?php
-		get_search_form(
-			array(
-				'label' => __( '404 not found', 'guide' ),
-			)
-		);
-		?>
+			<ul>
+				<li><a href="/">Home page</a></li>
+				<li><a href="/blog/">Latest blog posts</a></li>
+			</ul>
 
-	</div><!-- .section-inner -->
+			<br>
 
-</main><!-- #site-content -->
+			<p>Or try a search:</p>
+
+			<?php
+			get_search_form(
+				array(
+					'label' => __( '404 not found', 'guide' ),
+				)
+			);
+			?>
+
+		</div>
+
+	</div>
+
+</section>
 
 <?php
 get_footer();
